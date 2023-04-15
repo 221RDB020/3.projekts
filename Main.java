3508f85d9dec0a78576596e169c4bb488501bf8f
janis147 <jupitersaule@gmail.com>
2023-04-15 12:18:11 +0300

    structure

--- Main.java
+++ Main.java
@@ -18,14 +18,14 @@ public class Main {
         sourceFile = sc.next();
         System.out.print("archive name: ");
         resultFile = sc.next();
-        comp(sourceFile, resultFile);
+        new Compressor().comp(sourceFile, resultFile);
         break;
       case "decomp":
         System.out.print("archive name: ");
         sourceFile = sc.next();
         System.out.print("file name: ");
         resultFile = sc.next();
-        decomp(sourceFile, resultFile);
+        new Compressor().decomp(sourceFile, resultFile);
         break;
       case "size":
         System.out.print("file name: ");
@@ -50,31 +50,6 @@ public class Main {
     sc.close();
   }
 
-  public static void comp(String sourceFile, String resultFile) {
-    // counts frequency of each character
-    Map<Character, Integer> frequencyMap = new HashMap<>();
-
-    try {
-      FileInputStream inputFile = new FileInputStream(sourceFile);
-      int c;
-
-      while ((c = inputFile.read()) != -1) {
-        char character = (char) c;
-        frequencyMap.put(character, frequencyMap.getOrDefault(character,0) + 1);
-      }
-
-      System.out.println(frequencyMap);
-
-      inputFile.close();
-    } catch (IOException ex) {
-      System.out.println(ex.getMessage());
-    }
-  }
-
-  public static void decomp(String sourceFile, String resultFile) {
-    
-  }
-  
   public static void size(String sourceFile) {
     try {
       FileInputStream f = new FileInputStream(sourceFile);
@@ -122,9 +97,99 @@ public class Main {
   }
   
   public static void about() {
-    // TODO insert information about authors
     System.out.println("221RDB020 Jānis Žogots 12.grupa");
-    System.out.println("111RDB111 Ilze Programmētāja");
+    System.out.println("221RDB228 Lauris Senkāns 4.grupa");
+    System.out.println("221RDB063 Renārs Dambis 13.grupa");
+    System.out.println("221RDB334 Ronalds Jierkis 17.grupa");
+    System.out.println("221RDB136 Dainis Kudrjavcevs 1.grupa");
+    System.out.println("221RDB353 Pāvels Kudrjavcevs 2.grupa");
+  }
+  
+  static class Compressor {
+    public void comp(String sourceFile, String resultFile) {
+      try {
+          FileInputStream in = new FileInputStream(sourceFile);
+          FileOutputStream out = new FileOutputStream(resultFile);
+
+          byte[] inputBuffer = new byte[1024];
+          byte[] encodedBuffer = new byte[1024];
+
+          int bytesRead;
+          
+          ByteArrayOutputStream compressed = new ByteArrayOutputStream();
+
+          while ((bytesRead = in.read(inputBuffer)) != -1) {
+              byte[] encoded = new LZ77().encode(inputBuffer);
+              compressed.write(encoded);
+          }
+
+          HuffmanCoding huffman = new HuffmanCoding();
+
+          byte[] compressedBytes = compressed.toByteArray();
+          byte[] encodedHuffman = huffman.encode(compressedBytes);
+          
+          out.write(encodedHuffman);
+          in.close();
+          out.close();
+      } catch (IOException e) {
+          System.out.println(e.getMessage());
+      }
+    }
+
+    public void decomp(String sourceFile, String resultFile) {
+      //TODO: implement decomp algorythm
+    }
+  }
+
+  static class LZ77 {
+    public byte[] encode(byte[] data) {
+      return null;
+    }
+
+    public byte[] decode(byte[] data) {
+      return null;
+    }
+  }
+  
+  static class LZ77Token {
+    public LZ77Token(int matchOffset, int matchLength, byte b) {
+    }
+
+    public int getOffset() {
+      return 0;
+    }
+
+    public Byte getLiteral() {
+      return null;
+    }
+
+    public int getLength() {
+      return 0;
+    }
+  }
+
+  static class SuffixArray {
+    public static int byteArrayToInt(byte[] copyOfRange) {
+      return 0;
+    }
+
+    public static Collection<? extends Byte> intToByteArray(int offset) {
+      return null;
+    }
+  }
+
+  static class HuffmanCoding {
+    public byte[] encode(byte[] compressedBytes) {
+      return null;
+    }
+
+    public byte[] decode(byte[] compressedBytes) {
+      return null;
+    }
+    //TODO: implement HuffmanCoding algorythm
+  }
+
+  static class HuffmanTree {
+    //TODO: implement HuffmanTree algorythm
   }
 }
-
